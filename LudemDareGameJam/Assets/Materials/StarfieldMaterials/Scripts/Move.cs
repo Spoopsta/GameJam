79a93761df2f00b9c9c8429ff6762b7358d36adf
bunboii <44217176+bunboii@users.noreply.github.com>
2019-10-06 18:26:22 +1000

    main menu, tutorial, skyboxes.

---
+++ LudemDareGameJam/Assets/Materials/StarfieldMaterials/Scripts/Move.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    float Target;
+
+    public int Counter;
+	void Start()
+	{
+        print("Thanks for buying this, if you need any support, email support@dilapidatedmeow.com. " +
+            "Please note I cannot help with scripting related problems.");
+
+        Counter = 0;
+	}
+
+	void Update()
+	{
+        Debug.Log(Counter);
+        if (Counter < 1000)
+        {
+            Forward();
+            Counter++;
+        }
+
+        else if (Counter >= 1000)
+        {
+            Reverse();
+            Counter++;
+        }
+
+        else if (Counter > 2000)
+        {
+            Counter = 0;
+        }
+    }
+
+    private void Forward()
+    {
+        Target += Time.deltaTime / 125;
+
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, Target), 0.05f);
+    }
+
+    private void Reverse()
+    {
+        Target -= Time.deltaTime / 125;
+
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, Target), 0.05f);
+    }
+}
---
+++ LudemDareGameJam/Assets/Scripts/UI/SceneManagement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    public Animation UIAnimation1;
+    public Animation UIAnimation2;
+    public Animation UIANimation3;
+
+    public GameObject StartCanvas;
+    public GameObject UICanvas;
+
+
+    private void Start()
+    {
+        StartCanvas.gameObject.SetActive(true);
+        UICanvas.gameObject.SetActive(false);
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            StartCanvas.gameObject.SetActive (false);
+
+            UICanvas.gameObject.SetActive(true);
+        }
+    }
+
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene(sceneBuildIndex: 2);
+        }
+    }
+}
