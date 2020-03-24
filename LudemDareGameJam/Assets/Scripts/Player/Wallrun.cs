2ed48f4d59c59aca44544ad902ea7f7e2db3c37d
BunBoi <rawlingsb1998@gmail.com>
2020-03-24 15:40:40 +1000

    basic drop shadow implemented

---
+++ LudemDareGameJam/Assets/Scripts/Player/Wallrun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wallrun : MonoBehaviour
+{
+
+    //wall running in directions
+    private bool isWallR = false;
+    private bool isWallL = false;
+
+    private RaycastHit hitR;
+    private RaycastHit hitL;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
